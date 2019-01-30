f8fc7abb38e13d9bda41e7f979c6dac2340c2b21
Saint-Jean Germila <germila@noukod.com>
2019-01-30 15:59:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,13 +20,13 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		@post = Post.find(params[:id])
 	end
 
 	def update
-	@article = Article.find(params[:id])
-	@article.update(title: params[:title], description: params[:description])
-	redirect_to article_path(@article)
+	@post = Post.find(params[:id])
+	@post.update(title: params[:title], description: params[:description])
+	redirect_to article_path(@post)
 end
 
 end
