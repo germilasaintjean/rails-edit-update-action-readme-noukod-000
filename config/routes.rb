572cc57e809d8adb3589576bc05c40ec46e20fb3
Saint-Jean Germila <germila@noukod.com>
2019-01-30 15:50:01 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
       get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+          patch 'articles/:id', to: 'articles#update'
 end
